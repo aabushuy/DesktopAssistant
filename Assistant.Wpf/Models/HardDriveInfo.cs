f33c16c682ef2c86fc84d5ea108e1e0d5619b73f
Alexey Bushuev <aabushuy@hotmail.com>
2023-07-09 08:21:40 +0300

    Disk info added

---
+++ Assistant.Wpf/Models/HardDriveInfo.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+
+namespace Assistant.Wpf.Models
+{
+    public class HardDriveInfo
+    {
+        private DriveInfo DriveInfo { get; }
+
+        public string Name => $"{DriveInfo.VolumeLabel} ({DriveInfo.Name}:)";
+
+        public string SizeText => $"{GetDiskSizeText(DriveInfo.AvailableFreeSpace)} free of {GetDiskSizeText(DriveInfo.TotalSize)}";
+
+        public int Occupancy => (int)((DriveInfo.TotalSize - DriveInfo.AvailableFreeSpace) * 100 / DriveInfo.TotalSize);
+
+        public HardDriveInfo(DriveInfo driveInfo)
+        {
+            DriveInfo = driveInfo;
+        }
+
+        private string GetDiskSizeText(long bytes, int iteration = 0)
+        {
+            var size = bytes / 1024;
+            if (size > 0)
+            {
+                return GetDiskSizeText(size, ++iteration);
+            }
+
+            return iteration switch
+            {
+                0 => $"{bytes} bytes",
+                1 => $"{bytes} KB",
+                2 => $"{bytes} MB",
+                3 => $"{bytes} GB",
+                4 => $"{bytes} TB",
+                _ => throw new NotImplementedException(),
+            };
+        }
+    }
+}
---
+++ Assistant.Wpf/ViewModel/HardDiskViewModel.cs
@@ -0,0 +1,37 @@
+﻿using Assistant.Wpf.Models;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Assistant.Wpf.ViewModel
+{
+    public class HardDiskViewModel : TimerViewModel
+    {
+        private ObservableCollection<HardDriveInfo> _drives;
+        public ObservableCollection<HardDriveInfo> DriveInfos
+        { 
+            get => _drives;
+            set 
+            {
+                _drives = value;
+                RaisePropertyChanged(nameof(DriveInfos));
+            }
+        }
+
+        public HardDiskViewModel() : base(1000 * 60)
+        {
+            DriveInfos = new ObservableCollection<HardDriveInfo>();
+            DoLoop();
+        }
+
+        protected override async Task DoLoop()
+        {
+            var drives = DriveInfo
+                .GetDrives()
+                .Select(d => new HardDriveInfo(d));
+
+            DriveInfos = new ObservableCollection<HardDriveInfo>(drives);
+        }
+    }
+}
